27be3c2d37d9bc914780d5af608ba4975b7d6827
kellyFarfan <71039500+kellyFarfan@users.noreply.github.com>
2020-09-18 22:10:04 -0500

    Cambios en ciudad, provincia y tipo de vivienda

--- EnjoyHolidaysSpringbootApp/src/main/java/app/service/ProvinciaService.java
+++ EnjoyHolidaysSpringbootApp/src/main/java/app/service/ProvinciaService.java
@@ -1,7 +1,6 @@
 package app.service;
 
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +9,6 @@ import app.http.HttpCodeResponse;
 import app.http.HttpDescriptionResponse;
 import app.http.HttpListResponse;
 import app.http.HttpObjectResponse;
-import app.http.HttpSimpleResponse;
 import app.model.Provincia;
 import app.repository.ProvinciaRepository;
 
@@ -39,44 +37,5 @@ public class ProvinciaService {
                     null
             );
         }
-    }
-
-    public HttpObjectResponse<Provincia> crearProvincia(Provincia provincia) {
-        Optional<Provincia> provinciaBd = provinciaRepository.findById(provincia.getId());
-
-        if (provinciaBd != null) {
-            provinciaRepository.save(provincia);
-            return new HttpObjectResponse<>(
-                    HttpCodeResponse.CREATED,
-                    HttpDescriptionResponse.CREATED,
-                    provincia);
-        }else{
-            return new HttpObjectResponse<>(
-                    HttpCodeResponse.ENTIDAD_NO_ENCONTRADA,
-                    HttpDescriptionResponse.entidadNoEncontrada("provincia"),
-                    null);
-        }
-    }
-
-    public HttpSimpleResponse deleteProvincia(Long id) {
-        if(provinciaRepository.findById(id).isPresent()){
-            provinciaRepository.deleteById(id);
-            return new HttpSimpleResponse(HttpCodeResponse.OK, HttpDescriptionResponse.OK);
-        }else
-            return new HttpSimpleResponse(HttpCodeResponse.ENTIDAD_NO_ENCONTRADA, 
-                    HttpDescriptionResponse.entidadNoEncontrada("Provincia"));
-    }
-
-
-    public HttpObjectResponse<Provincia> updateProvincia(Provincia provincia) {
-        Long id = provincia.getId();
-        if(provinciaRepository.findById(id).isPresent()){
-            provinciaRepository.saveAndFlush(provincia);
-            return  new HttpObjectResponse<>(
-                    HttpCodeResponse.OK, HttpDescriptionResponse.OK, provincia);
-        }else
-            return  new HttpObjectResponse<>(
-                    HttpCodeResponse.ENTIDAD_NO_ENCONTRADA, 
-                    HttpDescriptionResponse.entidadNoEncontrada("Provincia"), null);
     }
 }
